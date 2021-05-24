43d7987c7e15edc2580881d721bba47133fae290
Rio358 <56245429+Rio358@users.noreply.github.com>
2021-05-24 09:47:47 -0400

    all the animals
    skinnable

--- client/main.lua
+++ client/main.lua
@@ -27,6 +27,30 @@ local animal = {
 	`a_c_rabbit_01`,
 	`a_c_rat`,
 	`a_c_cow`,
+	`a_c_cat`,
+	`a_c_chickenhawk`,
+	`a_c_chimp`,
+	`a_c_chop`,
+	`a_c_cormorant`,
+	`a_c_crow`,
+	`a_c_dolphin`,
+	`a_c_fish`,
+	`a_c_hen`,
+	`a_c_humpback`,
+	`a_c_husky`,
+	`a_c_killerwhale`,
+	`a_c_pigeon`,
+	`a_c_poodle`,
+	`a_c_pug`,
+	`a_c_retriever`,
+	`a_c_rhesus`,
+	`a_c_rottweiler`,
+	`a_c_seagull`,
+	`a_c_sharkhammer`,
+	`a_c_sharktiger`,
+	`a_c_shepherd`,
+	`a_c_stingray`,
+	`a_c_westy`,
 }
 
 -- Aminauls
@@ -70,7 +94,7 @@ RegisterNetEvent("hunting:butcherCreature")
 AddEventHandler("hunting:butcherCreature", function()
 	local dead = false
 	local closestAnimal, closestDistance = ESX.Game.GetClosestPed(plyCoords)
-	local animal = `a_c_cow`,`a_c_deer`, `a_c_boar`, `a_c_coyote`, `a_c_mtlion`, `a_c_pig`, `a_c_rabbit_01`, `a_c_rat` --dunno why but this seems to make it all work?
+	local animal = `a_c_cow`,`a_c_deer`,`a_c_boar`,`a_c_coyote`,`a_c_mtlion`,`a_c_pig`,`a_c_rabbit_01`,`a_c_rat`,`a_c_cat`,`a_c_chickenhawk`,`a_c_chimp`,`a_c_chop`,`a_c_cormorant`,`a_c_crow`,`a_c_dolphin`,`a_c_fish`,`a_c_hen`,`a_c_humpback`,`a_c_husky`,`a_c_killerwhale`,`a_c_pigeon`,`a_c_poodle`,`a_c_pug`,`a_c_retriever`,`a_c_rhesus`,`a_c_rottweiler`,`a_c_seagull`,`a_c_sharkhammer`,`a_c_sharktiger`,`a_c_shepherd`,`a_c_stingray`,`a_c_westy` --dunno why but this seems to make it all work?
 	local plyCoords = GetEntityCoords(PlayerPedId())
 	if closestAnimal ~= -1 and closestDistance <= 3.0 then
 		if GetPedType(closestAnimal) == 28 and GetEntityHealth(closestAnimal) == 0 and GetPedSourceOfDeath(closestAnimal) == PlayerPedId() then

3f979249c8c6f5a3ce1b36c6fb380cc94a366532
Rio358 <56245429+Rio358@users.noreply.github.com>
2021-05-25 23:11:06 -0400

    Add files via upload

---
+++ client/config.lua
@@ -0,0 +1,25 @@
+-- Credit to DRKM43 for his contribution and beautiful face.
+
+Config = {}
+
+Config.Invincible = true
+Config.Frozen = true
+Config.Stoic = true
+Config.FadeIn = true
+Config.DistanceSpawn = 20.0
+Config.Stress = false
+
+Config.MinusOne = true
+
+Config.GenderNumbers = {
+	['male'] = 4,
+	['female'] = 5
+}
+
+Config.PedList = {
+	{
+		model = `s_m_y_autopsy_01`, -- place holder, find a suitable ped and apply it here, adjust model flag in bt-targeting to relfect the new chosen model as well, there is a safety backup right behind the ped just incase.
+		coords = vector4(-69.77,6255.8,31.09, 208.48), -- Cluckin' Bell Factory Paleto Office.
+		gender = 'male' -- he's got balls.
+	},
+}
